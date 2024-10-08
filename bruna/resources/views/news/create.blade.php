a3a1075a15c9736c2c0568cd4977e23444f19285
caique <caiquedadonas@outlook.com>
2024-10-08 20:00:11 -0300

    upload main base files

---
+++ bruna/resources/views/news/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Publicar Noticia')
+
+@section('content')
+
+<h1>Publique uma noticia</h1>
+
+@endsection
---
+++ bruna/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'home')
+
+@section('content')
+
+
+@endsection
