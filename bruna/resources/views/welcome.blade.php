3002bd934871879820a149ec66a9941a34f86e0c
caique <caiquedadonas@outlook.com>
2024-10-10 19:01:15 -0300

    text execept hidden

--- bruna/resources/views/welcome.blade.php
+++ bruna/resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
            <div class="col">
             <div class="card">
                     <img src="https://picsum.photos/200/200?random=1" alt="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">card-title</h5>
+                    <div class="card-body text-hidden">
+                        <h5 class="card-title ">card-title</h5>
                         <h6 class="card-subtile">subtitle</h6>
                         <p class="card-text">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. In assumenda natus delectus dolorem earum numquam exercitationem illum. Laudantium voluptas veniam natus eveniet optio tempora dicta nam, rerum, sint ad earum!
@@ -29,7 +29,7 @@
         <div class="col">
             <div class="card">
                     <img src="https://picsum.photos/200/200?random=2" alt="" class="card-img-top">
-                    <div class="card-body">
+                    <div class="card-body text-hidden">
                         <h5 class="card-title">card-title</h5>
                         <h6 class="card-subtile">subtitle</h6>
                         <p class="card-text">
@@ -46,13 +46,13 @@
         <div class="col">
             <div class="card">
                     <img src="https://picsum.photos/200/200?random=3" alt="" class="card-img-top">
-                    <div class="card-body">
+                    <div class="card-body text-hidden">
                         <h5 class="card-title">card-title</h5>
                         <h6 class="card-subtile">subtitle</h6>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In assumenda natus delectus dolorem earum numquam exercitationem illum. Laudantium voluptas veniam natus eveniet optio tempora dicta nam, rerum, sint ad earum!
+                        <p class="card-text ">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In assumenda natus delectus dolorem earum numquam exercitationem illum. Laudantium voluptas veniam natus eveniet optio tempora dicta nam, rerum, sint ad earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, est deleniti eius perspiciatis commodi voluptatem numquam ea laborum doloribus sit perferendis architecto nobis eum corrupti unde, explicabo quisquam similique quibusdam!  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat expedita cumque laboriosam quasi illum illo similique natus atque. Aliquam blanditiis facere sit itaque fugiat omnis laboriosam maiores natus enim velit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa amet ab tempore odio labore consequatur iste aut architecto? Et libero iusto ipsum commodi, doloremque illo eligendi a facilis quibusdam nesciunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga voluptas delectus dolore aspernatur doloribus assumenda incidunt illum officiis dolores aliquid reprehenderit temporibus alias, recusandae tempore optio qui quo, voluptatem necessitatibus.
                         </p>
-                        <a href="#" class="card-button"><button type="button" class="btn btn-primary">Saiba mais</button></a>
+                        <a href="javascript:void();" class="card-button"><button type="button" class="btn btn-primary">Saiba mais</button></a>
                   </div>
 
              </div>
@@ -63,6 +63,11 @@
     </div>
     </div>
    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <script>
+
+    </script>
       
 @endforeach
 
